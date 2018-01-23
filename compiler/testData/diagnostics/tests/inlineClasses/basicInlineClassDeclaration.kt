cdc1af83e2d3f45f49b1110a28087e17b3919445
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-01-23 16:16:32 +0300

    Introduce `InlineClasses` language feature
     Allow to write `inline` modifier in front of class declaration

---
+++ compiler/testData/diagnostics/tests/inlineClasses/basicInlineClassDeclaration.kt
@@ -0,0 +1,7 @@
+// !LANGUAGE: +InlineClasses
+
+inline class Foo(val x: Int)
+
+<!WRONG_MODIFIER_TARGET!>inline<!> annotation class InlineAnn
+<!WRONG_MODIFIER_TARGET!>inline<!> object InlineObject
+<!WRONG_MODIFIER_TARGET!>inline<!> enum class InlineEnum
