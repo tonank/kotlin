122485f0a87d7e530644dc7d5f53ee5ffa5cfa22
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2018-01-24 14:33:30 +0300

    Basic inline class mapping to the underlying representation

--- core/descriptors.jvm/src/org/jetbrains/kotlin/load/kotlin/typeSignatureMapping.kt
+++ core/descriptors.jvm/src/org/jetbrains/kotlin/load/kotlin/typeSignatureMapping.kt
@@ -19,6 +19,7 @@ import org.jetbrains.kotlin.resolve.DescriptorUtils
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameUnsafe
 import org.jetbrains.kotlin.resolve.jvm.JvmClassName
 import org.jetbrains.kotlin.resolve.jvm.JvmPrimitiveType
+import org.jetbrains.kotlin.resolve.underlyingRepresentationForInlineClass
 import org.jetbrains.kotlin.types.*
 import org.jetbrains.kotlin.types.typeUtil.replaceArgumentsWithStarProjections
 import org.jetbrains.kotlin.utils.DO_NOTHING_3
@@ -128,6 +129,11 @@ fun <T : Any> mapType(
         }
 
         descriptor is ClassDescriptor -> {
+            if (descriptor.isInline && !mode.needInlineClassWrapping) {
+                val underlyingType = descriptor.underlyingRepresentationForInlineClass().type
+                return mapType(underlyingType, factory, mode, typeMappingConfiguration, descriptorTypeWriter, writeGenericType)
+            }
+
             val jvmType =
                 if (mode.isForAnnotationParameter && KotlinBuiltIns.isKClass(descriptor)) {
                     factory.javaLangClassType
---
+++ core/descriptors/src/org/jetbrains/kotlin/resolve/inlineClassesUtils.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.resolve
+
+import org.jetbrains.kotlin.descriptors.ClassDescriptor
+import org.jetbrains.kotlin.descriptors.ValueParameterDescriptor
+
+fun ClassDescriptor.underlyingRepresentation(): ValueParameterDescriptor? {
+    if (!isInline) return null
+    return unsubstitutedPrimaryConstructor?.valueParameters?.singleOrNull()
+}
+
+fun ClassDescriptor.underlyingRepresentationForInlineClass(): ValueParameterDescriptor {
+    if (!isInline) {
+        throw IllegalArgumentException("This method should be called only on class descriptor for inline classes")
+    }
+
+    return underlyingRepresentation()!!
+}
